fe5b6057fb3a7cdab5d4224b6cfd30be0ae4ff57
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-02-05 00:46:55 -0800

    cleaned tqdm more

--- src/train.py
+++ src/train.py
@@ -8,29 +8,30 @@ def train_net(net, data, epochs=1000):
 	# train net
 	loss_fn = torch.nn.MSELoss()
 	optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-	for i in tqdm(range(epochs)):
-		try:
-			# track epoch loss
-			tot_loss = 0
-			features, labels = data
-			# prepare for backprop
-			optimizer.zero_grad()
-			# compute prediction
-			output = net(features)
-			# print("out: {}\n\tlabel: {}\n".format(output, label))
-			# compute loss
-			loss = loss_fn(output, labels)
-			# compute loss gradient
-			loss.backward()
-			# update weights
-			optimizer.step()
-			# report loss
-			tot_loss += loss.item()
-			# report loss
-			avg_loss = tot_loss / float(len(data))
-			print(" Epoch {} Avg Loss: {}\n".format(i, avg_loss))
-		except:
-			print("Interrupted.")
-			return net
+	with tqdm(range(epochs)) as t:
+		for i in t:
+			try:
+				# track epoch loss
+				tot_loss = 0
+				features, labels = data
+				# prepare for backprop
+				optimizer.zero_grad()
+				# compute prediction
+				output = net(features)
+				# print("out: {}\n\tlabel: {}\n".format(output, label))
+				# compute loss
+				loss = loss_fn(output, labels)
+				# compute loss gradient
+				loss.backward()
+				# update weights
+				optimizer.step()
+				# report loss
+				tot_loss += loss.item()
+				# report loss
+				avg_loss = tot_loss / float(len(data))
+				t.write(" Epoch {} Avg Loss: {}\n".format(i, avg_loss))
+			except:
+				print("Interrupted.")
+				return net
 	print('\n')
 	return net
