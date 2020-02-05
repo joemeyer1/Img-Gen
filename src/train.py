bb606a483c0091249a961739d54424e721b2a228
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-02-05 00:00:40 -0800

    reset tqdm instead of re-initializing

--- src/train.py
+++ src/train.py
@@ -8,11 +8,11 @@ def train_net(net, data, epochs=1000):
 	# train net
 	loss_fn = torch.nn.MSELoss()
 	optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-	for i in range(epochs):
-		try:
-			# track epoch loss
-			tot_loss = 0
-			with tqdm(total=len(data)) as t:
+	with tqdm(total=len(data)) as t:
+		for i in range(epochs):
+			try:
+				# track epoch loss
+				tot_loss = 0
 				for features, label in data:
 					features = features.reshape([1]+list(features.shape))
 					label = label.reshape(1)
@@ -30,11 +30,12 @@ def train_net(net, data, epochs=1000):
 					# report loss
 					tot_loss += loss.item()
 					t.update()
-			# report loss
-			avg_loss = tot_loss / float(len(data))
-			print("Epoch {} Avg Loss: {}\n".format(i, avg_loss))
-		except:
-			print("Interrupted.")
-			return net
+				# report loss
+				avg_loss = tot_loss / float(len(data))
+				print("Epoch {} Avg Loss: {}\n".format(i, avg_loss))
+			except:
+				print("Interrupted.")
+				return net
+			t.reset()
 	print('\n')
 	return net
