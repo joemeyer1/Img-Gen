40356b21f0af44267b8ba9ad532f70792e966d58
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-02-04 22:39:43 -0800

    allow user interrupt for training

--- src/train.py
+++ src/train.py
@@ -9,36 +9,32 @@ def train_net(net, data, epochs=1000):
 	loss_fn = torch.nn.MSELoss()
 	optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 	for i in range(epochs):
-		# try:
-		# track epoch loss
-		tot_loss = 0
-		# i = 0
-		with tqdm(total=len(data)) as t:
-			for features, label in data:
-				features = features.reshape([1]+list(features.shape))
-				label = label.reshape(1)
-				# prepare for backprop
-				optimizer.zero_grad()
-				# compute prediction
-				output = net(features)[0]
-				# print("out: {}\n\tlabel: {}\n".format(output, label))
-				# compute loss
-				loss = loss_fn(output, label)
-				# compute loss gradient
-				loss.backward()
-				# update weights
-				optimizer.step()
-				# report loss
-				tot_loss += loss.item()
-				t.update()
-				# if i%(len(data)//10) == 0:
-				# 	print('{}.'.format(i), end='', flush=True)
-				# i += 1
-		# report loss
-		avg_loss = tot_loss / float(len(data))
-		print("Epoch {} Avg Loss: {}\n".format(i, avg_loss))
-		# except:
-		# 	print("Interrupted.")
-		# 	return net
+		try:
+			# track epoch loss
+			tot_loss = 0
+			with tqdm(total=len(data)) as t:
+				for features, label in data:
+					features = features.reshape([1]+list(features.shape))
+					label = label.reshape(1)
+					# prepare for backprop
+					optimizer.zero_grad()
+					# compute prediction
+					output = net(features)[0]
+					# print("out: {}\n\tlabel: {}\n".format(output, label))
+					# compute loss
+					loss = loss_fn(output, label)
+					# compute loss gradient
+					loss.backward()
+					# update weights
+					optimizer.step()
+					# report loss
+					tot_loss += loss.item()
+					t.update()
+			# report loss
+			avg_loss = tot_loss / float(len(data))
+			print("Epoch {} Avg Loss: {}\n".format(i, avg_loss))
+		except:
+			print("Interrupted.")
+			return net
 	print('\n')
 	return net
