43445169600757502fc7651f760952e983e5037b
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-02-10 03:57:53 -0800

    batch data

--- src/batch_data.py
+++ src/batch_data.py
@@ -1,8 +1,26 @@
 
 from numpy.random import shuffle
+from math import ceil
 import torch
 
 def batch(data, batch_size):
+	feats, labs = data
+	indices = torch.randperm(len(feats))
+	batches = []
+	for batch_n in range(len(feats)//batch_size):
+		features, labels = [], []
+		batch_inds = indices[batch_n*batch_size:(batch_n+1)*batch_size]
+		for i in batch_inds:
+			feature, label = feats[i], labs[i]
+			features.append(feature)
+			labels.append(label)
+		batches.append((features, labels))
+	return batches
+
+
+
+
+def batch_split(data, batch_size):
 	feats, labs = data
 	indices = torch.randperm(len(feats))
 	feature_batches, label_batches = [], []
--- src/train.py
+++ src/train.py
@@ -2,32 +2,37 @@
 # trains net
 import torch
 from tqdm import tqdm
+from src.batch_data import batch as get_batches
 
-def train_net(net, data, epochs=1000):
+def train_net(net, data, epochs=1000, batch_size = 100):
 
 	# train net
 	loss_fn = torch.nn.MSELoss()
-	optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-	with tqdm(range(epochs)) as t:
-		for i in t:
-			try:
-				features, labels = data
-				# prepare for backprop
-				optimizer.zero_grad()
-				# compute prediction
-				output = net(features)
-				# print("out: {}\n\tlabel: {}\n".format(output, label))
-				# compute loss
-				loss = loss_fn(output, labels)
-				# compute loss gradient
-				loss.backward()
-				# update weights
-				optimizer.step()
-				# report loss
-				avg_loss = loss.item() / float(len(features))
-				t.write(" Epoch {} Avg Loss: {}\n".format(i, avg_loss))
-			except:
-				print("Interrupted.")
-				return net
+	optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+	with tqdm(range(epochs)) as epoch_counter:
+		for epoch in epoch_counter:
+			tot_batch_loss = 0
+			for batch in get_batches(data, batch_size):
+				try:
+					features, labels = batch
+					# prepare for backprop
+					optimizer.zero_grad()
+					# compute prediction
+					output = net(features)
+					# print("out: {}\n\tlabel: {}\n".format(output, label))
+					# compute loss
+					loss = loss_fn(output, labels)
+					# compute loss gradient
+					loss.backward()
+					# update weights
+					optimizer.step()
+					# report loss
+					tot_batch_loss += loss.item()
+				except:
+					print("Interrupted.")
+					return net
+			# report loss
+			avg_loss = tot_batch_loss / float(batch_size)
+			epoch_counter.write(" Epoch {} Avg Loss: {}\n".format(epoch, avg_loss))
 	print('\n')
 	return net
