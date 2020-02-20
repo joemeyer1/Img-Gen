f28ef953d3a6055eb3fe919387989cb6565a8567
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-02-19 22:29:10 -0800

    batch counter during training

--- src/train.py
+++ src/train.py
@@ -10,37 +10,39 @@ def train_net(net, data, epochs=1000, batch_size = 100, verbose=True, lr=.001, s
 	loss_fn = torch.nn.MSELoss()
 	optimizer = torch.optim.Adam(net.parameters(), lr=lr)
 	best_net, min_loss = None, -float('inf')
-	with tqdm(range(epochs), desc='0') as epoch_counter:
+	with tqdm(range(epochs)) as epoch_counter:
 		try:
 			for epoch in epoch_counter:
 				tot_batch_loss = 0
 				batches = get_batches(data, batch_size)
-				for batch_i in range(len(batches)):
-					batch = batches[batch_i]
-					features, labels = batch
-					# prepare for backprop
-					optimizer.zero_grad()
-					# compute prediction
-					output = torch.sigmoid(net(features))
-					# print("out: {}\n\tlabel: {}\n".format(output, labels))
-					# compute loss
-					loss = loss_fn(output, labels)
-					# compute loss gradient
-					loss.backward()
-					# update weights
-					optimizer.step()
-					# report loss
-					tot_batch_loss += loss.item()
-					if verbose and batch_i > 0:
-						running_loss = tot_batch_loss / float(batch_i+1)
-						epoch_counter.desc = str(running_loss)
-						epoch_counter.refresh()
-						# epoch_counter.write("\t Epoch {} Running Loss: {}\n".format(epoch, running_loss))
+				with tqdm(range(len(batches))) as batch_counter:
+					for batch_i in batch_counter:
+						batch = batches[batch_i]
+						features, labels = batch
+						# prepare for backprop
+						optimizer.zero_grad()
+						# compute prediction
+						output = torch.sigmoid(net(features))
+						# print("out: {}\n\tlabel: {}\n".format(output, labels))
+						# compute loss
+						loss = loss_fn(output, labels)
+						# compute loss gradient
+						loss.backward()
+						# update weights
+						optimizer.step()
+						# report loss
+						tot_batch_loss += loss.item()
+						if verbose and batch_i > 0:
+							running_loss = tot_batch_loss / float(batch_i+1)
+							batch_counter.desc = str(running_loss)
+							batch_counter.refresh()
+							# epoch_counter.write("\t Epoch {} Running Loss: {}\n".format(epoch, running_loss))
 				# report loss
 				avg_loss = tot_batch_loss / float(len(batches))
 				epoch_counter.write(" Epoch {} Avg Loss: {}\n".format(epoch, avg_loss))
 				if save_best_net and avg_loss < min_loss:
 					best_net, min_loss = net.copy(), avg_loss.copy()
+				epoch_counter.desc = str(avg_loss)
 
 		except:
 			print("Interrupted.")
