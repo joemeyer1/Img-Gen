05e5052947b4bd9b6c9619cfc9845efd55c116c9
joemeyer1 <47130044+joemeyer1@users.noreply.github.com>
2020-02-20 00:19:57 -0800

    batch data differently, minor bugs fixed

--- src/batch_data.py
+++ src/batch_data.py
@@ -1,37 +1,67 @@
 
 from numpy.random import shuffle
-from math import ceil
-import torch
+# import torch
 
-def batch(data, batch_size):
-	feats, labs = data
-	indices = torch.randperm(len(feats))
+# def batch(data, batch_size):
+# 	feats, labs = data
+# 	indices = torch.randperm(len(feats))
+# 	batches = []
+# 	for batch_n in range(len(feats)//batch_size):
+# 		features, labels = [], []
+# 		batch_inds = indices[batch_n*batch_size:(batch_n+1)*batch_size]
+# 		for i in batch_inds:
+# 			feature, label = feats[i], labs[i]
+# 			features.append(feature)
+# 			labels.append(label)
+# 		features, labels = torch.stack(features), torch.stack(labels)
+# 		batches.append((features, labels))
+# 	return batches
+
+
+
+
+# def batch_split(data, batch_size):
+# 	feats, labs = data
+# 	indices = torch.randperm(len(feats))
+# 	feature_batches, label_batches = [], []
+# 	for batch_n in range(len(feats)//batch_size):
+# 		features, labels = [], []
+# 		batch_inds = indices[batch_n*batch_size:(batch_n+1)*batch_size]
+# 		for i in batch_inds:
+# 			feature, label = feats[i], labs[i]
+# 			features.append(feature)
+# 			labels.append(label)
+# 		feature_batches.append(features)
+# 		label_batches.append(labels)
+# 	return feature_batches, label_batches
+
+
+
+def batch(pos_data, neg_data, batch_size):
+	pos_data = partition(pos_data, batch_size//2)
+	neg_data = partition(neg_data, len(pos_data))
 	batches = []
-	for batch_n in range(len(feats)//batch_size):
-		features, labels = [], []
-		batch_inds = indices[batch_n*batch_size:(batch_n+1)*batch_size]
-		for i in batch_inds:
-			feature, label = feats[i], labs[i]
-			features.append(feature)
-			labels.append(label)
-		features, labels = torch.stack(features), torch.stack(labels)
-		batches.append((features, labels))
+	for i in range(len(pos_data)):
+		batch = pos_data[i]+neg_data[i]
+		shuffle(batch)
+		batches.append(batch)
+	return batches
+
+def partition(data, batch_size):
+	shuffle(data)
+	batches = []
+	i = 0
+	while i+batch_size < len(data):
+		batch = data[i:i+batch_size]
+		batches.append(batch)
+		i += batch_size
 	return batches
 
 
 
 
-def batch_split(data, batch_size):
-	feats, labs = data
-	indices = torch.randperm(len(feats))
-	feature_batches, label_batches = [], []
-	for batch_n in range(len(feats)//batch_size):
-		features, labels = [], []
-		batch_inds = indices[batch_n*batch_size:(batch_n+1)*batch_size]
-		for i in batch_inds:
-			feature, label = feats[i], labs[i]
-			features.append(feature)
-			labels.append(label)
-		feature_batches.append(features)
-		label_batches.append(labels)
-	return feature_batches, label_batches
+
+
+
+
+
